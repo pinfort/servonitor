1e89e66c79337ac1d098b57749ddaff96447e631
pinfort <ptg@nijitei.com>
2019-07-10 19:41:37 +0900

    Httpgetクラスを書き始めた

---
+++ src/main/kotlin/me/pinfort/servonitor/commands/http/Get.kt
@@ -0,0 +1,21 @@
+package me.pinfort.servonitor.commands.http
+
+import me.pinfort.servonitor.commands.Commands
+
+class Get: Commands() {
+    override fun execute(hostName: String, args: Map<String, String>?): Boolean {
+        val config: Map<String, String> = config(args ?: mapOf())
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    fun config(args: Map<String, String>): Map<String, String> {
+        val code: String = if (args.containsKey("expectedCode")) {
+            args["expectedCode"] ?: "200"
+        } else {
+            "200"
+        }
+
+
+        return mapOf()
+    }
+}
